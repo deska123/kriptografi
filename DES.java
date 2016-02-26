c5ed4272c36fb10af2166a3d96b563d9d54ff801
deska123 <dkamah913@gmail.com>
2016-02-26 15:47:11 +0700

    Menambah fungsi FinalPermutation
    Membuat fungsi baru yaitu FinalPermutation

--- DES.java
+++ DES.java
@@ -121,6 +121,26 @@ public class DES
         return baru;
     }
     
+    public static char[] finalPermutation(char[] teks)
+    {
+        char[] baru = new char[teks.length];
+        int[] ip = 
+        {39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28,
+        34, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26,
+        33, 1, 41, 9, 49, 17, 57, 25,
+        32, 0, 40, 8, 48, 16, 56, 24};
+        
+        char[] temp = teks;
+        for(int i = 0; i < baru.length; i++) {
+            baru[i] = teks[ip[i]];
+        }
+        return baru;
+    }
+    
     public static char[] pc1 (char[] kunci)
     {
         char[] baru = new char[kunci.length - 8];
